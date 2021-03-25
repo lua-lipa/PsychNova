b77f1c40ccc6d656158cb4ad583505804b5af36b
eronaaliu <50829986+eronaaliu@users.noreply.github.com>
2021-03-25 11:44:01 +0000

    insert into user

---
+++ sql/insert_into_user.sql
@@ -0,0 +1,57 @@
+INSERT INTO user(
+                first_name,
+                last_name,
+                date_of_birth,
+                gender,
+                profession,
+                current_location,
+                profile_picture,
+                description,
+                email,
+                password,
+                mobile_number,
+                type
+        )
+VALUES (
+                'Jennifer',
+                'Lopez',
+                '2000-12-06',
+                'Female',
+                'Palm Reader',
+                'Spain',
+                'hi',
+                'A novice palm reader looking for employment in the Psychic industry! Great interpersonal and communincation skills.',
+                'jen@gmail.com',
+                'test123',
+                '3646363',
+                'user';
+);
+
+INSERT INTO user(
+                first_name,
+                last_name,
+                date_of_birth,
+                gender,
+                profession,
+                current_location,
+                profile_picture,
+                description,
+                email,
+                password,
+                mobile_number,
+                type
+        )
+VALUES (
+                'Tia',
+                'Mowry',
+                '2000-12-06',
+                'Female',
+                'Fortune Teller',
+                'America',
+                'hi',
+                'An experienced Fortune Teller focusing on helping my clients focus on their futures and learn from their potential mistakes. Great interpersonal skills',
+                'tia@gmail.com',
+                'test123',
+                '4456345',
+                'user';
+);
