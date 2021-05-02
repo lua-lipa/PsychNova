286774f851bb0d57ef7bbed8e1ce22d4a726acb6
eronaaliu <50829986+eronaaliu@users.noreply.github.com>
2021-05-03 00:48:16 +0100

    Profile Editing - Add/Remove Skills

--- classes/userSkills.php
+++ classes/userSkills.php
@@ -3,10 +3,25 @@
 class userSkills
 {
     //inner join
-    public function getUserSkills($userid)
+    public function getUserSkills($userId)
     {
-        $query = "SELECT * FROM user_skills INNER JOIN skills ON skills.skill_id=user_skills.skill_id WHERE user_id=$userid";
+        $query = "SELECT * FROM user_skills INNER JOIN skills ON skills.skill_id=user_skills.skill_id WHERE user_id=$userId";
         $db = new Database();
         return $db->readById($query, "skill_id");
+    }
+
+    public function removeAllSkills($userId)
+    {
+        $query = "DELETE FROM user_skills WHERE user_id=$userId;";
+        $db = new Database();
+        return $db->save($query);
+    }
+
+    public function addUserSkill($userId, $skillId)
+    {
+        $query = "INSERT INTO user_skills(user_id, skill_id)
+                    VALUES ($userId, $skillId);";
+        $db = new Database();
+        return $db->save($query);
     }
 }
