c2b91b028ccbc42c02ea0d1c79c150555c6aab58
johnnyaokay <49286386+johnnyaokay@users.noreply.github.com>
2021-05-03 12:15:06 +0100

    Pulled main

--- classes/search.php
+++ classes/search.php
@@ -1,10 +1,12 @@
 <?php
 
-class Search {
+class Search
+{
 
-    
 
-    public function searchDatabase($data) {
+
+    public function searchDatabase($data)
+    {
         $searchInput = $data['searchinput'];
         $dropdownInput = $data['dropdowninput'];
 
@@ -19,7 +21,8 @@ class Search {
         }
     }
 
-    public function searchUsers($searchInput) {
+    public function searchUsers($searchInput)
+    {
         $query = "SELECT * FROM user WHERE
                     first_name LIKE '%$searchInput' ||
                     last_name LIKE '%$searchInput'";
@@ -27,7 +30,8 @@ class Search {
         return $db->read($query);
     }
 
-    public function searchUsersWithOptions($searchInput, $skillInput, $organisationInput) {
+    public function searchUsersWithOptions($searchInput, $skillInput, $organisationInput)
+    {
         //todo
         //search with skill only
         //search with companies worked for only
@@ -40,12 +44,11 @@ class Search {
         return $db->read($query);
     }
 
-    public function searchOrganisations($searchInput) {
+    public function searchOrganisations($searchInput)
+    {
         $query = "SELECT * FROM organisation WHERE
                     name LIKE '%$searchInput'";
         $db = new Database();
         return $db->read($query);
     }
-
-    
 }
