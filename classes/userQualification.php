8090219ceaf07354c1814f30d54fd00396c8d5f0
eronaaliu <50829986+eronaaliu@users.noreply.github.com>
2021-05-03 18:37:57 +0100

    editing profile

--- classes/userQualification.php
+++ classes/userQualification.php
@@ -8,4 +8,22 @@ class userQualification
         $db = new Database();
         return $db->read($query);
     }
+
+    public function addUserQualification($data)
+    {
+        $userId = $data['userId'];
+        $qualificationId = $data['qualificationId'];
+        $dateObtained = $data['dateObtained'];
+
+        $query =
+            "INSERT INTO user_qualification(
+            user_id,
+            qualification_id,
+            date_obtained
+            )
+            VALUES ($userId, $qualificationId, $dateObtained);";
+
+        $db = new Database();
+        return $db->read($query);
+    }
 }
