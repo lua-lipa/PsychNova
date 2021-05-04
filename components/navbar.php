de52218cb6b026284985b0cc14ddbacee4bf72f7
johnnyaokay <49286386+johnnyaokay@users.noreply.github.com>
2021-05-04 19:49:19 +0100

    Updated Profile CSS

--- components/navbar.php
+++ components/navbar.php
@@ -107,7 +107,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                         <a class="nav-link" href="jobs.php">Jobs</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="profile.php">Profile</a>
+                        <a class="nav-link" href="<?php echo "profile.php?userid= " . $_SESSION['userid'] ?> ">Profile</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="myorganisations.php">My Organisations</a>
