aeb437c052c03c21333602c58c6b5c13d9a5464e
Ranya Hwigi <ranya@Ranyas-MacBook-Pro.local>
2021-05-04 22:10:53 +0100

    adding user suggested vacancies

--- classes/vacancy.php
+++ classes/vacancy.php
@@ -1,12 +1,16 @@
 <?php
-
+include("skills.php");
 class vacancy
 {
-
+    
     public function getVacancies()
     {
         $query = "SELECT * FROM vacancy";
         $db = new Database();
         return $db->read($query);
+    }
+
+    public function suggestedVacancies($userId){
+
     }
 }
