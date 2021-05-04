f7abe78cf94fae2dab3d9ba3f258dc5a29ce3281
nutsachichilidze <36604389+nutsachichilidze@users.noreply.github.com>
2021-05-04 16:24:55 +0100

    apply button fix

--- html/timeline.php
+++ html/timeline.php
@@ -199,7 +199,7 @@ $recommendedVacancies = $vacancy->getVacancies();
                   <div class="vacancy-title">
                     <h9><?php echo $vacancyOrgData['name'] ?></h9><br>
                     <h9><?php echo $value['title'] ?></h9>
-                    <a href="mailto:<?php echo str_replace(' ', '', $vacancyOrgData['name']) ?>@psychnova.com?subject=Job Application" class="btn-small float-center" target=" _blank" rel="noopener noreferrer">Apply</a>
+                    <a href="mailto:<?php echo str_replace(' ', '', $vacancyOrgData['name']) ?>@psychnova.com?subject=Job Application" class="btn-small float-center" target="https://jobs.ie/" rel="noopener noreferrer">Apply</a>
 
                   </div>
 
