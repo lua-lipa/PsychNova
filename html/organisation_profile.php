d0a10ec285bf34145fd88b194773904f04494d55
Ranya Hwigi <ranya@Ranyas-MacBook-Pro.local>
2021-05-05 02:41:06 +0100

    fixed organisation profile

--- html/organisation_profile.php
+++ html/organisation_profile.php
@@ -43,10 +43,17 @@ if (isset($_POST['addVacancy'])) {
     }
 }
 
+if (isset($_POST['updateAbout'])) {
+    $org = new organisation();
+    $org->updateAbout($_GET['id'], $_POST);
+}
+
 // $organisationData = $organisation->getOrganisationData($_SESSION['userid']);
 $organisationData = $organisation->getOrganisationData($org_id);
 $user = new User();
 $userData = $user->getUserData($_SESSION['userid']);
+$loggedInUserData = $user->getUserData($_SESSION['userid']);
+$userType = $loggedInUserData['type'];
 
 $vacancy = new vacancy();
 $recommendedVacancies = $vacancy->getVacancies();
@@ -75,6 +82,7 @@ $selectedSkills = array();
 
 $vacancy = new vacancy();
 $organisationVacancies = $vacancy->getVacanciesOrg($org_id);
+$recommendedVacancies = $vacancy->suggestedVacancies($_SESSION['userid']);
 // echo "<pre>";
 // print_r($organisationVacancies);
 // echo "</pre>";
@@ -101,25 +109,30 @@ $organisationVacancies = $vacancy->getVacanciesOrg($org_id);
                     <div class="col-3 d-flex justify-content-center align-items-center">
                         <img src="<?php echo $organisationData['profile_picture'] ?>" class="img-circle" />
                     </div>
-                    <div class="col-9">
-                        <div class="row float-right">
-                            <button type="button" style="margin-right: 10px; margin-top: 10px; background-color: #ffffff; color: #000000; height: 35px; border-color: #876e8f; border-radius:50px;" class="btn btn-primary" data-toggle="modal" data-target="#editAboutModal">
-                                <i class="bi bi-pencil"></i>
-                            </button>
+                    <div class="col-6 pl-3 d-flex justify-content-center align-items-start flex-column">
+                        <div class="row">
                             <div class="modal fade" id="editAboutModal" tabindex="-1" role="dialog">
                                 <div class="modal-dialog" role="document">
                                     <div class="modal-content">
                                         <div class="modal-header">
-                                            <h5 class="modal-title" style="color:black">Edit About</h5>
+                                            <h5 class="modal-title" style="color:black">Edit Organisation</h5>
                                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                                 <span aria-hidden="true">&times;</span>
                                             </button>
                                         </div>
                                         <form action="" method="POST">
                                             <div class="modal-body">
+                                                <label for="name" style="color:black">Name</label> <input class="form-control" type="text" name="name" style="border-radius:5px;" value=<?php echo $organisationData['name'] ?> />
+                                                <br>
+                                                <label for="dateEstablished" style="color:black">Date Established</label> <input class="form-control" type="date" name="dateEstablished" style="border-radius:5px;" value="<?php echo $organisationData['date_established'] ?>" />
+                                                <br>
+                                                <label for="email" style="color:black">Email</label> <input class="form-control" type="text" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" title="Must be in the form psychnova@email.com" style="border-radius:5px;" value=<?php echo $organisationData['email'] ?> />
+                                                <br>
+                                                <label for="contact_number" style="color:black">Contact No.</label> <input class="form-control" type="text" name="contactNo" pattern="[0-9]{10}" title="Must be 10 numbers." style="border-radius:5px;" value=<?php echo $organisationData['contact_number'] ?> />
+                                                <br>
+                                                <label for="description" style="color:black">Description</label> <input class="form-control" type="text" name="description" style="margin-bottom:10px; height: 50px; width:95%; border-radius:5px;" maxlength="200" value="<?php echo $organisationData['description'] ?>" />
                                             </div>
                                             <div class="modal-footer">
-                                                <button type="button" class="btn btn-secondary" data-dismiss="modal" style="border-radius:15px; background-color: #876e8f; border-color:#876e8f">Close</button>
                                                 <button type="submit" name="updateAbout" class="btn btn-primary" style="border-radius:15px; background-color: #a58aae; border-color:#876e8f">Save changes</button>
                                             </div>
                                         </form>
@@ -128,29 +141,45 @@ $organisationVacancies = $vacancy->getVacanciesOrg($org_id);
                             </div>
                         </div>
                         <div class="row">
-                            <h4 class="user-name"><?php echo $organisationData['name'] ?></h4>
+                            <h4 class="user-name"><?php echo $organisationData['name']?></h4>
                         </div>
                         <div class="row">
-                            <p class="astro-sun"><i class="bi bi-sun"></i> <?php echo calcStarSign($userData['date_of_birth']) ?> </p>
+                            <p class="astro-sun"><i class="bi bi-sun"></i> <?php echo calcStarSign($organisationData['date_established']) ?> </p>
                             <p class="astro-rising"><i class="bi bi-sunrise"></i> <?php echo calcRising() ?></p>
                             <p class="astro-moon"><i class="bi bi-moon"></i> <?php echo calcMoon() ?></p>
                         </div>
                         <div class="row">
-                            <p class="user-connections">Contact Number: <?php echo $organisationData['contact_number'] ?> </p>
+                            <p class="user-connections">Email: <?php echo $organisationData['email'] ?> </p>
                         </div>
                         <div class="row">
-                            <p class="user-connections">Email: <?php echo $organisationData['contact_number'] ?> </p>
+                            <p class="user-connections">Contact Number: <?php echo $organisationData['contact_number'] ?> </p>
                         </div>
                     </div>
+                    <div class="col-3 d-flex flex-column justify-content-between">
+                            <?php
+                            if ($userType == 'administrator' || $_SESSION['userid'] == $organisationData['user_id']) {
+                            ?>
+                                <div class="row d-flex justify-content-end">
+                                    <button type="button" style="margin-right: 10px; margin-top: 10px; background-color: #ffffff; color: #000000; height: 35px; border-color: #876e8f; border-radius:50px;" class="btn btn-primary" data-toggle="modal" data-target="#editAboutModal">
+                                        <i class="bi bi-pencil"></i>
+                                    </button>
+                                </div>
+                            <?php
+                            }
+                            ?>
+                    </div> 
                 </div>
                 <div class="row h-22 mt-3">
-                    <div class="mb-3">
-                        <h8 class="mb-3">About</h8>
+                        <div class="col">
+                            <div class="row mt-2">
+                                <h8 class="mb-2">About</h8>
+                            </div>
+                            <div class="row card-about" ]>
+                                <p style=""><?php echo $organisationData['description'] ?></p>
+                            </div>
+                        </div>
+
                     </div>
-                    <div class="card-about px-4 pt-4 pb-4">
-                        <p style="font-size: 14px"><?php echo $organisationData['description'] ?></p>
-                    </div>
-                </div>
 
                 <!-- ADD Vacancy -->
                 <div class="row mt-3">
@@ -315,38 +344,42 @@ $organisationVacancies = $vacancy->getVacanciesOrg($org_id);
             <!-- Vacancies -->
             <div class="col-lg-3">
                 <div class="vacancies-card">
-                    <h class="connections-title" style="text-align:center">Recommended Vacancies</h>
-                    <?php if (count($recommendedVacancies) == 0) {
-                        echo "no vacancies to show.";
-                    } else {
-                        $numberOfVacanciesDisplayed = 0;
-                        foreach ($recommendedVacancies as $key => $value) {
-                            if ($numberOfVacanciesDisplayed == 4) break;
-                            else {
-                                $vacancyOrgData = $organisation->getOrganisationData($value['org_id']);
-                                $numberOfVacanciesDisplayed += 1;
-                            }
-                    ?>
-                            <div class="connection-row">
-                                <div class="vacancy-header">
-                                    <img src=https://dummyimage.com/40x40/cfcfcf/000000 class="rounded-circle" alt="...">
-                                    <div class="vacancy-title">
-                                        <h9><?php echo $vacancyOrgData['name'] ?></h9><br>
-                                        <h9><?php echo $value['title'] ?></h9><br>
-                                    </div>
-                                </div>
-                                <h9><?php echo $value['description'] ?></h9><br>
-                                <h9><?php echo "Requirements: " . $value['required_experience'] ?></h9><br>
-                                <br>
-                                <a class="btn-small float-right" href="jobs.php">Apply</a>
-
-                                <br>
-                            </div>
+                <h class="connections-title" style="text-align:center; font-size: 12px"><b>Recommended Vacancies</b></h>
+                <br>
+                <?php if (count($recommendedVacancies) == 0) { ?>
+                    <p style="text-align:center">No vacancies to show</p><br>
                     <?php
-                        }
+                } else {
+                    $numberOfVacanciesDisplayed = 0;
+                    foreach ($recommendedVacancies as $key => $value) {
+                    if ($numberOfVacanciesDisplayed == 3) break;
+                    else {
+                        $vacancyOrgData = $organisation->getOrganisationData($value['org_id']);
+                        $numberOfVacanciesDisplayed += 1;
                     }
+
                     ?>
-                    <a class="btn-small float-center" href="jobs.php"> More</a>
+                    <div class="connection-row">
+                        <div class="vacancy-header">
+                        <img src="<?php echo $vacancyOrgData['profile_picture'] ?>" width="60" height="60" class=" rounded-circle" alt="...">
+                        <div class="vacancy-title">
+                            <h9><a style="color: #A58AAE; text-decoration: none;" href="organisation_profile.php?userid=<?php echo $vacancyOrgData['org_id'] ?>" type="submit" name="view"><?php echo $vacancyOrgData['name'] ?></a></h9><br>
+                            <h9><?php echo $value['title'] ?></h9>
+                            <a href="mailto:<?php echo str_replace(' ', '', $vacancyOrgData['name']) ?>@psychnova.com?subject=Job Application" class="btn-small float-center" target="https://jobs.ie/" rel="noopener noreferrer">Apply</a>
+
+                        </div>
+
+                        </div>
+                        <h style="font-size:12px"><i><?php echo $value['description'] ?></i></h><br>
+                        <hr>
+                    </div>
+                    <!-- <a class="btn-small float-center" style="margin-top:5px" href="jobs.php">Apply</a> -->
+
+                <?php
+                    }
+                }
+                ?>
+                <a class="btn-view-more float-center" href="suggested_vacancies.php">Explore</a>
                 </div>
             </div>
 
