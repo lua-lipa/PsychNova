f5fdda2045701f887757bd66e455cb60f721d43b
Ranya Hwigi <ranya@Ranyas-MacBook-Pro.local>
2021-05-05 13:54:43 +0100

    updated suggested vacancies to include skills

--- html/suggested_vacancies.php
+++ html/suggested_vacancies.php
@@ -66,7 +66,21 @@ $recommendedVacancies = $vacancy->suggestedVacancies($_SESSION['userid']);
                                         <div class="row result-card-description">
                                             <h7><?php echo $value['description'] ?></h7>
                                         </div>
-                                        
+                                        <div class="row result-card-date">
+                                            <h9 class="mt-3">Required Skills</h9><br>
+                                        </div>
+                                        <div class="row">
+                                            <?php
+                                            $vacancySkillsData = $vacancy->vacancySkills($value['vacancy_id']);
+                                            foreach ($vacancySkillsData as $key => $value) {
+                                            ?>
+                                                <div class="skills" style="flex-wrap: wrap;">
+                                                    <span type="badge badge-primary" class="badge-skill" style="background-color: #876e8f;  margin-bottom:20px;"><?php echo $value['title'] ?> </span>
+                                                </div>
+                                            <?php
+                                            }
+                                            ?>
+                                        </div>
                                     </div>
                                     
                                 </div>
